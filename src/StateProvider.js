6d3c9740f05a50bc41a1e9f912c059022f4bcd93
NirmalKumarNK08 <nirmalkumar.nk008@gmail.com>
2021-10-11 20:06:24 +0530

    Minute changes(wip)

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,10 +1,9 @@
 import React, { createContext, useContext, useReducer } from "react";
-import { render } from "react-dom";
 
 export const StateContext = createContext();
 
 export const StateProvider = ({ reducer, initialState, children }) => {
-   return (
+   return(
       <StateContext.Provider value={useReducer(reducer, initialState)}>
          {children}
       </StateContext.Provider>
